c6c139b920ba2a549e7c4c28723c49e53677d06c
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-10-03 23:10:27 -0300

    adicionando aula 11

---
+++ curso-basico-java/aula 11/curso-java-basico/src/com/cursojava/basico/aula11/VariaveisFlutuantes.java
@@ -0,0 +1,10 @@
+package com.cursojava.basico.aula11;
+
+public class VariaveisFlutuantes {
+    public static void main(String[] args){
+        double valorDaPassagem = 2.90;
+        float valorTomate = 3.95f;
+        System.out.println("Valor da passagem: " + valorDaPassagem );
+        System.out.println("Valor do tomate: " + valorTomate );
+    }
+}
